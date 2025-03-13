84e2197852e1b3e3706063f39619f97ef52d216a
TrailCodes <aryandalvi0715@gmail.com>
2025-03-13 20:34:43 +0530

    Handle the None Exception

--- shell.py
+++ shell.py
@@ -4,5 +4,9 @@ while True:
     text = input('FLUX > ')
     result, error = FLUX.run('<stdin>', text)
 
-    if error: print(error.as_string())
-    else: print(result)
+    if error:
+        print(error.as_string())
+    elif result is None:
+        print("An unexpected error occurred, no result returned.")
+    else:
+        print(result)
